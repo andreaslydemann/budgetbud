ac2bd1f8072426b67de754529f1c6d768cad5ece
Andreas Lüdemann <andreaslydemann@gmail.com>
2018-03-13 18:24:21 +0100

    Add initial files for debt

---
+++ src/actions/debt_actions.js
@@ -0,0 +1,26 @@
+
+/*
+import {AsyncStorage} from 'react-native';
+import {firebaseFunctionsURL} from "../config/firebase_config";
+import firebase from 'firebase';
+import axios from 'axios';
+
+import {
+    AUTH_SCREEN_RESET,
+    CPR_NUMBER_CHANGED,
+} from './types';
+
+const ROOT_URL = firebaseFunctionsURL;
+
+export const authScreenSwitched = (callback) => async dispatch => {
+    dispatch({type: AUTH_SCREEN_RESET});
+    callback();
+};
+
+export const cprNumberChanged = text => {
+    return {
+        type: CPR_NUMBER_CHANGED,
+        payload: text
+    };
+};
+*/
